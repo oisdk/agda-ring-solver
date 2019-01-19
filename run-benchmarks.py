784d2d76db4bcb66aa54e1a0ebb93b4e19bc7850
Donnacha Oisín Kidney <mail@doisinkidney.com>
2019-01-19 16:15:02 +0000

    lovely graphs

--- run-benchmarks.py
+++ run-benchmarks.py
@@ -26,17 +26,18 @@ open +-*-Solver
 """
 
 expressions = [
-    ("(v + w + x + y + z) ^ d", list(range(1,9))),
     ("v ^ d + w ^ d + x ^ d + y ^ d + z ^ d",[1,100,200,300,400,500]),
-    ("(k 1 + v ^ 1 + w ^ 2 + x ^ 3 + y ^ 4 + z ^ 5) ^ d",list(range(1,9)))
+    ("(k1 + v ^ 1 + w ^ 2 + x ^ 3 + y ^ 4 + z ^ 5) ^ d",list(range(1,9))),
+    ("(v + w + x + y + z) ^ d", list(range(1,9))),
 ]
 
-dense_expr_encoding = str.maketrans({'+' : ':+', '^' : ':^', '*' : ':*', 'k' : 'con'})
+dense_expr_encoding = str.maketrans({'+' : ':+', '^' : ':^', '*' : ':*', 'k' : 'con '})
 sparse_expr_encoding = str.maketrans({'k': ''})
 
 import subprocess
 import os
 import time
+import sys
 
 def line_by_line(command):
     return subprocess.check_output(command, universal_newlines=True).split('\n')
@@ -44,9 +45,12 @@ def line_by_line(command):
 for expr_, degrees in expressions:
     header = expr_.translate(sparse_expr_encoding)
     print(header)
-    print('%-3s | %-7s | %-7s |' % ('d', 'sparse', 'dense'))
+    print('%3s | %-7s | %-7s |' % ('d', 'sparse', 'dense'))
+    sparse_results = []
+    dense_results = []
     for degree in degrees:
         print((('%3i | ' % degree)), end='')
+        sys.stdout.flush()
         expr = expr_.replace('d', str(degree))
         pretty_expr = expr.translate(sparse_expr_encoding)
         try:
@@ -62,7 +66,10 @@ for expr_, degrees in expressions:
             before = time.time()
             subprocess.run(['agda', '--no-syntactic-equality', 'src/Benchmarks.agda'], capture_output=True, check=True)
             after = time.time()
-            print(('%7g | ' % (after-before)), end='')
+            res = after-before
+            print(('%7g | ' % res), end='')
+            sys.stdout.flush()
+            sparse_results.append(res)
         finally:
             os.remove('src/Benchmarks.agda')
         try:
@@ -78,6 +85,33 @@ for expr_, degrees in expressions:
             before = time.time()
             subprocess.run(['agda', '--no-syntactic-equality', 'src/Benchmarks.agda'], capture_output=True, check=True)
             after = time.time()
-            print(('%7g | ' % (after-before)))
+            res = after-before
+            print(('%7g | ' % res))
+            sys.stdout.flush()
+            dense_results.append(res)
         finally:
             os.remove('src/Benchmarks.agda')
+    print()
+
+    width = 9 + len(degrees) * 10
+    print('-' * width)
+    max_res = max(sparse_results+dense_results)
+    sparse_ys = [ int((y * 39) / max_res) for y in sparse_results ]
+    dense_ys  = [ int((y * 39) / max_res) for y in dense_results ]
+    for y in reversed(range(40)):
+        print('%5i | ' % int((y / 40) * max_res), end='')
+        for y_s, y_d in zip(sparse_ys, dense_ys):
+            if y == y_d:
+                print('*' + ' ' * 9, end='')
+            elif y == y_s:
+                print('+' + ' ' * 9, end='')
+            else:
+                print(' ' * 10, end='')
+        print('|')
+    max_deg = max(degrees)
+    print('-' * width)
+    print(' ' * 4, end='d = ')
+    for x in range(len(degrees)):
+        print(('%-10i' % round((x / (len(degrees))) * max_deg)), end='')
+    print()
+    print()
