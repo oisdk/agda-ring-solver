71cd8fd9b2b7760e0689bb4b5a6f78a81bd1e02b
Donnacha Oisín Kidney <mail@doisinkidney.com>
2019-01-22 18:42:08 +0000

    lemmas

--- run-benchmarks.py
+++ run-benchmarks.py
@@ -56,7 +56,7 @@ def expr_3(n):
 
 
 expressions = [
-    (expr_1, list(range(8, 9))),
+    (expr_1, list(range(5, 9))),
     (expr_2, [1, 100, 200, 300, 400, 500]),
     (expr_3, list(range(1, 9))),
 ]
