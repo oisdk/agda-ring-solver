ef646e6ac90622cea2bcc32e887db57ddb8cd9d5
Donnacha Oisín Kidney <mail@doisinkidney.com>
2019-01-23 14:40:38 +0000

    better benchmarks

--- run-benchmarks.py
+++ run-benchmarks.py
@@ -54,11 +54,10 @@ def expr_3(n):
     return '(1 + ' + ' + '.join('x%i ^ %i' % (i, i)
                                 for i in range(1, n + 1)) + ') ^ d'
 
-
 expressions = [
-    (expr_1, list(range(8, 9))),
-    (expr_3, list(range(7, 8))),
-    # (expr_2, [1, 100, 200, 300, 400, 500]),
+    (expr_1, '(x1 + x2 + ... + xn) ^ d'),
+    (expr_2, 'x1 ^ d + x2 ^ d + ... + xn ^ d'),
+    (expr_3, '(1 + x1 ^ 1 + x2 ^ 2 + ... + xn ^ n) ^ d'),
 ]
 
 import sympy
@@ -112,6 +111,7 @@ def time_file_typecheck(contents):
             print('\n' + proc.stdout)
             raise err
 
+import itertools
 
 def print_graph(degrees, sparse_results, dense_results):
     width = 9 + len(degrees) * 10
@@ -121,7 +121,7 @@ def print_graph(degrees, sparse_results, dense_results):
     dense_ys = [int((y * 40) / max_res) for y in dense_results]
     for y in range(39, -1, -1):
         print('%5i | ' % int((y / 40) * max_res), end='')
-        for y_s, y_d in zip(sparse_ys, dense_ys):
+        for y_s, y_d in itertools.zip_longest(sparse_ys, dense_ys):
             if y == y_d:
                 print('*' + ' ' * 9, end='')
             elif y == y_s:
@@ -137,43 +137,52 @@ def print_graph(degrees, sparse_results, dense_results):
         print('%-10i' % round(deg), end='')
     print()
 
-
-for n in range(5, 9):
-    varnames = ' '.join('x' + str(i) for i in range(1, n + 1))
-    for expr_fn, degrees in expressions:
-        expr_ = expr_fn(n)
-        print(expr_)
-        print('%3s | %-7s | %-7s |' % ('d', 'sparse', 'dense'))
-        sparse_results = []
-        dense_results = []
-        for degree in degrees:
-            print((('%3i | ' % degree)), end='')
-            sys.stdout.flush()
-            expr = expr_.replace('d', str(degree))
-            typesig = 'lemma : ∀ %s → %s ≈ %s' % (varnames, expr, expand(expr))
-            res = time_file_typecheck('\n'.join((sparse_preamble, typesig,
-                                                 'lemma = solve NatRing')))
-            print(('%7g | ' % res), end='')
-            sys.stdout.flush()
-            sparse_results.append(res)
-            typesig = 'lemma : ∀ %s → %s ≡ %s' % (varnames, expr, expand(expr))
-            expr_ast = 'lemma = solve %i (λ %s → %s := %s ) refl' % (
-                n, varnames, expr.translate(dense_expr_encoding),
-                expand(expr).translate(dense_expr_encoding))
-            res = 2
-            # res = time_file_typecheck('\n'.join((dense_preamble, typesig,
-            #                                      expr_ast)))
-            print(('%7g | ' % res))
-            sys.stdout.flush()
-            dense_results.append(res)
-        print()
-        print_graph(degrees, sparse_results, dense_results)
-        print()
-        with open('benchmark-logs', 'a') as logfile:
-            logfile.write(str(datetime.datetime.now()) + '\n')
-            logfile.write(expr_ + '\n')
-            logfile.write('d sparse dense\n')
-            for d, sp, dn in zip(degrees, sparse_results, dense_results):
-                logfile.write(d.__repr__() + ' ' + sp.__repr__() + ' ' +
-                              dn.__repr__() + '\n')
-            logfile.write('\n\n')
+print('Choose an expression: ')
+for i, (_, expr_desc) in enumerate(expressions):
+    print('%2i : %s' % (i, expr_desc))
+expr_fn = expressions[int(input('> '))][0]
+n = int(input('Choose n:\n> '))
+degrees = [ int(d) for d in input('Choose degrees (default= 1 2 3 4 5 6 7 8)\n> ').split() ] or list(range(1,9))
+benches = input('Choose things to benchmark (default = sparse dense)\n> ').split() or ['sparse', 'dense']
+varnames = ' '.join('x' + str(i) for i in range(1, n + 1))
+expr_ = expr_fn(n)
+print(expr_)
+print('%3s | %-7s | %-7s |' % ('d', 'sparse', 'dense'))
+sparse_results = []
+dense_results = []
+for degree in degrees:
+    print((('%3i | ' % degree)), end='')
+    sys.stdout.flush()
+    expr = expr_.replace('d', str(degree))
+    if 'sparse' in benches:
+        typesig = 'lemma : ∀ %s → %s ≈ %s' % (varnames, expr, expand(expr))
+        res = time_file_typecheck('\n'.join((sparse_preamble, typesig,
+                                                'lemma = solve NatRing')))
+        print(('%7g | ' % res), end='')
+        sparse_results.append(res)
+    else:
+        print('...     | ', end='')
+    sys.stdout.flush()
+    if 'dense' in benches:
+        typesig = 'lemma : ∀ %s → %s ≡ %s' % (varnames, expr, expand(expr))
+        expr_ast = 'lemma = solve %i (λ %s → %s := %s ) refl' % (
+            n, varnames, expr.translate(dense_expr_encoding),
+            expand(expr).translate(dense_expr_encoding))
+        res = time_file_typecheck('\n'.join((dense_preamble, typesig,
+                                            expr_ast)))
+        print(('%7g | ' % res))
+        dense_results.append(res)
+    else:
+        print('...     | ')
+    sys.stdout.flush()
+print()
+print_graph(degrees, sparse_results, dense_results)
+print()
+with open('benchmark-logs', 'a') as logfile:
+    logfile.write(str(datetime.datetime.now()) + '\n')
+    logfile.write(expr_ + '\n')
+    logfile.write('d sparse dense\n')
+    for d, sp, dn in itertools.zip_longest(degrees, sparse_results, dense_results):
+        logfile.write(d.__repr__() + ' ' + sp.__repr__() + ' ' +
+                        dn.__repr__() + '\n')
+    logfile.write('\n\n')
